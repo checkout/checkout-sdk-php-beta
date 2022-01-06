91dad4cbb58d3999e4028e48e73abeed29aa7791
Armando Ibarra <aibarrakerzon@gmail.com>
2022-01-05 21:10:51 -0600

    (CS1/CS2) Add Support for Customers

--- lib/Checkout/Four/CheckoutApi.php
+++ lib/Checkout/Four/CheckoutApi.php
@@ -4,19 +4,27 @@ namespace Checkout\Four;
 
 use Checkout\ApiClient;
 use Checkout\CheckoutConfiguration;
+use Checkout\Customers\Four\CustomersClient;
 use Checkout\Tokens\TokensClient;
 
 final class CheckoutApi
 {
     private TokensClient $tokensClient;
+    private CustomersClient $customersClient;
 
     public function __construct(ApiClient $apiClient, CheckoutConfiguration $configuration)
     {
         $this->tokensClient = new TokensClient($apiClient, $configuration);
+        $this->customersClient = new CustomersClient($apiClient, $configuration);
     }
 
     public function getTokensClient(): TokensClient
     {
         return $this->tokensClient;
+    }
+
+    public function getCustomersClient(): CustomersClient
+    {
+        return $this->customersClient;
     }
 }
