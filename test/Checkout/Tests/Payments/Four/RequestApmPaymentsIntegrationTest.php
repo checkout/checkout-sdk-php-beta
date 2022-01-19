f296d5af22cc20f4b4801f5fb131d1317bb312b9
Bonafede <bonafedeit@gmail.com>
2022-01-19 11:41:42 -0300

    (CS2) Add support alternative payments

---
+++ test/Checkout/Tests/Payments/Four/RequestApmPaymentsIntegrationTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Checkout\Tests\Payments\Four;
+
+use Checkout\CheckoutApiException;
+use Checkout\Common\Currency;
+use Checkout\Payments\Four\Request\PaymentRequest;
+use Checkout\Payments\Four\Request\Source\Apm\RequestIdealSource;
+use Checkout\Payments\Four\Request\Source\Apm\RequestSofortSource;
+use Checkout\Payments\Four\Request\Source\RequestCardSource;
+use Checkout\Tests\TestCardSource;
+
+class RequestApmPaymentsIntegrationTest extends AbstractPaymentsIntegrationTest
+{
+    /**
+     * @test
+     * @throws CheckoutApiException
+     */
+    public function shouldMakeIdealPayment(): void
+    {
+        $requestSource = new RequestIdealSource();
+        $requestSource->bic = "INGBNL2A";
+        $requestSource->description = "ORD50234E89";
+        $requestSource->language = "nl";
+
+        $paymentRequest = new PaymentRequest();
+        $paymentRequest->source = $requestSource;
+        $paymentRequest->capture = true;
+        $paymentRequest->amount = 1000;
+        $paymentRequest->currency = Currency::$EUR;
+        $paymentRequest->success_url = "https://testing.checkout.com/sucess";
+        $paymentRequest->failure_url = "https://testing.checkout.com/failure";
+
+        $paymentResponse1 = $this->fourApi->getPaymentsClient()->requestPayment($paymentRequest, $this->idempotencyKey);
+
+        self::assertResponse($paymentResponse1,
+            "id",
+            "amount",
+            "currency",
+            "status",
+            "response_code",
+            "response_summary",
+            "_links",
+            "_links.self");
+
+        $paymentResponse2 = $this->fourApi->getPaymentsClient()->getPaymentDetails($paymentResponse1["id"]);
+
+        self::assertResponse($paymentResponse2,
+            "status",
+            "_links",
+            "_links.self",
+            "source",
+            "source.type");
+    }
+
+    /**
+     * @test
+     * @throws CheckoutApiException
+     */
+    public function shouldMakeSofortPayment(): void
+    {
+        $requestSource = new RequestSofortSource();
+
+        $paymentRequest = new PaymentRequest();
+        $paymentRequest->source = $requestSource;
+        $paymentRequest->capture = true;
+        $paymentRequest->amount = 100;
+        $paymentRequest->currency = Currency::$EUR;
+        $paymentRequest->success_url = "https://testing.checkout.com/sucess";
+        $paymentRequest->failure_url = "https://testing.checkout.com/failure";
+
+        $paymentResponse1 = $this->fourApi->getPaymentsClient()->requestPayment($paymentRequest, $this->idempotencyKey);
+        self::assertResponse($paymentResponse1,
+            "id",
+            "amount",
+            "currency",
+            "status",
+            "response_code",
+            "response_summary",
+            "_links",
+            "_links.self");
+
+        $paymentResponse2 = $this->fourApi->getPaymentsClient()->requestPayment($paymentRequest, $this->idempotencyKey);
+        self::assertResponse($paymentResponse2,
+            "status",
+            "_links",
+            "_links.self",
+            "source",
+            "source.type");
+    }
+}
